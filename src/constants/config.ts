b2ae2e585a236989a94bbe0da24b60337e9dbd20
Your Name <you@example.com>
2025-06-03 19:05:28 +0300

    this is My Portoflio

---
+++ src/constants/config.ts
@@ -0,0 +1,95 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Eman Mohamed",
+    fullName: "Eman Mohamed",
+    email: "eman.m22omer@gmail.com",
+  },
+  hero: {
+    name: "Eman Mohamed",
+    p: ["I'm Frontend React Developer"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `
+            I’m a Front-End Developer with expertise in React, JavaScript, and TypeScript. 
+            I specialize in creating intuitive and efficient user interfaces using modern web technologies
+            like HTML, CSS, and Node.js. With experience in SQL Server and MongoDB, I’m equipped to build
+            scalable solutions. I excel both independently and as part of a team, always focused on delivering
+            innovative solutions that meet client needs.`,
+            },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,242 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend NodeJs Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "Web Developer",
+    companyName: "Emdad",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "December 2024 - March 2025",
+    points: [
+      "Developed a responsive bilingual website (Arabic & English) for marketing military and civilian equipment. ",
+      "Built UI using HTML, CSS, and JavaScript; integrated with backend (.NET MVC) for dynamic data display. ",
+      "Implemented QR code functionality to bridge print and digital materials, enhancing user access. ",
+      "Optimized mobile layout, reducing bounce rate by 20%. ",
+    ],
+  },
+  {
+    title: "ReactJS Developer",
+    companyName: "Guitara Group",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "March 2025 - June 2025",
+    points: [
+      "Developed and deployed the agency’s official website using React and Vite, increasing page speed by 40%.",
+      "Integrated front-end with back-end systems via NodeJS in collaboration with backend team. ",
+      "Co-developed a Shopify-based e-commerce platform with a dynamic price comparison feature, improving user engagement by 25%.",
+      "Designed a branded portfolio website showcasing agency services and events. ",
+      "Ensured responsive, accessible design across all platforms, contributing to 30% increase in mobile traffic.",
+    ],
+  },
+ 
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "Tree Project",
+    description:
+      "Created an educational platform on environmental awareness using MERN stack (MongoDB, Express, React, Node.js).",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://github.com/EngEman01/final-project/tree/master",
+  },
+  {
+    name: "Tech Nook Store ",
+    description:
+      "Built a React.js e-commerce platform with API integration for real-time product listings.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "api",
+        color: "green-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://github.com/EngEman01/TechNook",
+  },
+  {
+    name: "Tammy Food ",
+    description:
+      "Developed a responsive recipe website using HTML and CSS. Focused on user-friendly navigation and mobile optimization. ",
+    tags: [
+      {
+        name: "html",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "https://github.com/EngEman01/Tammy-Food",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
